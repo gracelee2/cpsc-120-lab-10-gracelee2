bdc16d80db1526fcb587ba537fafe0bfad591b46
Grace <grace1@csu.fullerton.edu>
2021-12-02 13:35:46 -0800

    done

--- part-1/addressbook.cc
+++ part-1/addressbook.cc
@@ -24,58 +24,33 @@ int main(int argc, char* argv[]) {
   /// Number of enteries in the address book.
   const int kEnteries = 3;
   /// The address book is C++ Vector object that contains Person objects.
-  // TODO: declare a variable named addressbook which is a vector of
-  // Person objects.
   vector<Person> addressbook;
 
   cout << "Let's add " << kEnteries << " to your address book!\n";
   for (int i = 0; i < kEnteries; i++) {
-    // TODO: Print out a prompt for the computer user to enter a person's name
-    // TODO: Declare a string variable named `name` to store the person's name.
-    // TODO: Using getline(), read from cin and store into `name`; read up to
-    // '\n'.
-
-    cout << "Please enter a full name: " << "";
+    cout << "Please enter a full name: "
+         << "";
     string name = "";
-    cin >> name;
     getline(cin, name);
 
-    // TODO: Print out a prompt for the computer user to enter a
-    // person's phone number.
-    // TODO: Declare a string variable named `phone_number` to store
-    // the person's phone number.
-    // TODO: Using getline(), read from cin and store into `phone_number`;
-    // read up to '\n'.
-
-    cout << "Please enter a phone number: " << "";
+    cout << "Please enter a phone number: "
+         << "";
     string phone_number = "";
-    cin >> phone_number;
     getline(cin, phone_number);
 
-    // TODO: Declare a variable named `p` which is of type Person. Intialize
-    // it with the name and phone number that was read in from cin.
-    // TODO: Use the push_back() method to insert into the vector the person
-    // that was just created.
-    // https://en.cppreference.com/w/cpp/container/vector/push_back
     Person p;
     p = Person(name, phone_number);
     addressbook.push_back(p);
   }
   cout << "Let's sort your addressbook!\n";
   sort(addressbook.begin(), addressbook.end());
-  // TODO: Sort the address_book vector using sort().
-  // https://en.cppreference.com/w/cpp/algorithm/sort
 
   cout << "Great! Let's print out all the enteries in your addressbook.\n";
-  //auto args = vector<string>(argv, argv + argc);
-  for(const auto& address : addressbook){
-	cout << address << "\n";
-  // TODO: Using a range-for loop, print out each entry of the addressbook.
-  // Remember to use the overloaded << operator to output the person to
-  // cout.
+  // auto args = vector<string>(argv, argv + argc);
+  for (const auto& address : addressbook) {
+	  cout << address << "\n";
+  }
+  cout << "\n";
 
-}
-cout << "\n";
-
-return 0;
+  return 0;
 }
